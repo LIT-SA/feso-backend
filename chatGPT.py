3412a0cdbc3bc807a0454950e89039a7802ffe85
Ntholi Nkhatho <ntholi@live.com>
2023-05-03 08:52:35 +0200

    initial commit

---
+++ chatGPT.py
@@ -0,0 +1,31 @@
+from typing import Optional
+import logging
+import openai
+
+openai.api_key = "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"
+model_engine = "text-davinci-003"
+
+
+class ChatGPT:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def get_completion(prompt: str):
+        try:
+            response = openai.Completion.create(
+                engine=model_engine,
+                prompt=prompt,
+                max_tokens=2048,
+                n=1,
+                stop=None,
+                temperature=0.6,
+                timeout=10
+            )
+            text = response.choices[0].text.strip()
+            if len(text) == 0:
+                raise Exception("API response did not contain any text")
+            return text
+        except Exception as e:
+            logging.error(f"Error occurred during API call: {e}")
+            return ""
